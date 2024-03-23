93e8265b99ff960bd19a753e02e4a8ec3a4b66ac
Anna M <annamr324@gmail.com>
2024-03-23 19:16:12 +0100

    main modificat i comentat una mica mes generem arxiu amb les posicions finals he posat el calcul de la temp instantanea a dins el bucle
    No funciona el bucle es queda aturat i no tira

--- serial/src/main.f90
+++ serial/src/main.f90
@@ -6,7 +6,7 @@ Program main
    use :: pbc_module
 
    Implicit none
-   real(8), parameter :: mass = 1, rho = 0.7, epsilon = 1, sigma = 1
+   real(8), parameter :: mass = 1, rho = 0.7, epsilon = 1, sigma = 1 ! (LJ units, input file)
 !        real(8), parameter :: k_b = 1.380649e-23
    integer, parameter :: N = 125
    real(8), dimension(N, 3) :: r, r_ini, vel, vel_ini, r_out, v_fin
@@ -30,7 +30,7 @@ Program main
    M = N**(1./3.)
    a = L/(M)
 
-   print *, L, M, a
+   print *,"L =", L,"M =", M,"a=", a
 
    cutoff = 2.5
 
@@ -44,11 +44,21 @@ Program main
    print *, absV
 
    open (22, file="vel_ini.dat")
+   open (33, file="pos_ini.dat")
+   open (55, file="pos_out.dat")
 
    call initialize_positions(N, rho, r_ini)
 
+   ! Write initial positions to file
+   do i = 1, N
+      write (33, *) r_ini(i, :)
+   end do
+
+   close (33)
+
    call initialize_velocities(N, absV, vel_ini)
 
+   ! Write initial velocities to file
    do i = 1, N
       write (22, *) vel_ini(i, :)
    end do
@@ -59,41 +69,60 @@ Program main
    open (77, file="Temperatures_verlet.dat")
    open (23, file="vel_fin_Verlet.dat")
 
+   ! Time parameters, initial and final time (input.txt)
    tini = 0
-   tfin = 1
+   tfin = 10
 
    ! Apply Verlet algorithm
    write (44, *) ""
    write (44, *) ""
    write (44, *) "dt = ", dt
+   write (44, *) "#  time , pot, kin , total , momentum"
+   write (77, *) "#  time , Temp"
    print *, "dt = ", dt
+   print*,  "# time , pot, kin , total , momentum"
 
    Nsteps = int((tfin - tini)/dt)
 
    ! We roll back to the initial positions and velocities to initialize
    r = r_ini
    vel = vel_ini
+   
 
    do step = 1, Nsteps
-
+   
+      print*, "step = ", step
       call time_step_vVerlet(r, vel, pot, N, L, cutoff, dt)
       call kinetic_energy(vel, K_energy, N)
       call momentum(vel, p, N)
+      ! Calculate temperature
+      Temp = inst_temp(N, K_energy)
+      write (77, *) step*dt, Temp
       write (44, *) step*dt, pot, K_energy, pot + K_energy, p
       !        print*, K_energy
       if (mod(step, 1000) .eq. 0) then
          print *, real(step)/Nsteps
       end if
-
+      
+      ! We save the last 10% positions and velocity components of the simulation
       if (real(step)/Nsteps .gt. 0.9) then
          v_fin = v_fin + vel
+         r_out = r_out + r
       end if
+
 
    end do
 
-   Temp = inst_temp(N, K_energy)
-   write (77, *) "Verlet", dt, Temp
+   ! Write final positions to file to plot the distribution of positions
+   write (55, *) "#  Positions components (x, y, z) for the last 10% of the simulation"
+   write (55, *) "#  x, y, z"
+   do i = 1, N
+      write (55, *) r_out(i, :)/(Nsteps*0.1)
+   end do   
 
+   ! Write final velocities to file to plot the distribution of velocities
+   write (23, *) "#  Velocities components (x, y, z) and modulus (v) for the last 10% of the simulation"
+   write (23, *) "#  v_x, v_y, v_z, v"
    do i = 1, N
       write (23, *) v_fin(i, :)/(Nsteps*0.1), (v_fin(i, 1)**2 + v_fin(i, 2)**2 + v_fin(i, 3)**2)**(1./2.)/(Nsteps*0.1)
    end do
@@ -101,7 +130,7 @@ Program main
    write (23, *) ""
    write (23, *) ""
 
-
+   close (55)
    close (44)
    close (23)
    close (77)
