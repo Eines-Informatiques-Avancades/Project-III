c4c2748e31586d006867776498cab9fde8807894
root <root@PortatilAnna>
2024-03-25 22:31:51 +0100

    implementation of radial dist function

--- serial/src/plots/plots.py
+++ serial/src/plots/plots.py
@@ -11,6 +11,51 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 energy = np.loadtxt('../../energy_verlet.dat', skiprows=4, dtype=float)
 temperature = np.loadtxt('../../Temperatures_verlet.dat', skiprows=1, dtype=float)
 pressure = np.loadtxt('../../pressure_verlet.dat', skiprows=1, dtype=float)
+pos_fin = np.loadtxt('../../pos_out.dat', skiprows=1, dtype=float)
+
+print(np.shape(pos_fin)) # (N, 3)
+
+# Compute the radial distribution function
+def rdf(pos, L, N, dr):
+    # Number of particles
+    n = len(pos)
+    # Number of bins
+    nbins = int(L/(2*dr))
+    # Initialize the histogram
+    hist = np.zeros(nbins)
+    # Loop over all pairs of particles
+    for i in range(n):
+        for j in range(i+1, n):
+            # Compute the distance between particles i and j
+            rij = pos[i] - pos[j]
+            # Apply periodic boundary conditions
+            rij = rij - L * np.rint(rij/L)
+            r = np.linalg.norm(rij)
+            # Increment the histogram
+            if r < L/2:
+                k = int(r/dr)
+                hist[k-1] += 2
+    # Normalize the histogram
+    rho = n / (L**3)
+    for i in range(nbins):
+        r = (i + 0.5) * dr
+        hist[i] /= 4 * np.pi * r**2 * dr * rho * N
+    return hist
+
+#plot the radial distribution function
+L = 10.77
+N = 125
+dr = 0.1
+rdf = rdf(pos_fin, L, N, dr)
+r = np.linspace(0.5*dr, L/2-0.5*dr, len(rdf))
+plt.figure()
+plt.plot(r, rdf, label='Radial Distribution Function', color='mediumseagreen')
+plt.xlabel('r')
+plt.ylabel('g(r)')
+plt.legend()
+plt.title('Radial Distribution Function')
+plt.savefig('Radial_Distribution_function.png')
+#plt.show()
 
 
 #
