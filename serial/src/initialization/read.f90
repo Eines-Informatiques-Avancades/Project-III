5641e021e344246ff46db0536465f93998f97325
Manel <mserraro244@alumnes.ub.edu>
2024-04-16 09:20:33 +0200

    Tractament de dades afegit a subrutines v1

---
+++ serial/src/initialization/read.f90
@@ -0,0 +1,9 @@
+module read_write
+
+    contains
+
+    subroutine read_file(param_file, mass, epsilon, sigma, n_steps, timestep, density, &
+         n_write, n_frames, temperature)
+
+
+end module
