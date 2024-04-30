be8b32a5747fd02163ad2f2c457dbb68cb4b2893
Manel <mserraro244@alumnes.ub.edu>
2024-04-30 21:10:29 +0200

    Creació de plots de l'eficiència

--- parallel/src/plots/procs.py
+++ parallel/src/plots/procs.py
@@ -7,40 +7,39 @@ import os
 # Set path to the directory where the script is located
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-data = np.loadtxt('../../nprocs.dat', skiprows=1)
+data = np.loadtxt('./nprocs.dat', skiprows=1)
 
 processors = data[:, 0]                 # Cores
 time = data[:, 1]                       # Time taken by the simulation in seconds
 speedup = data[:,2]                     # Speedup
 efficiency = speedup / processors       # Efficiency
 
-# Plot time taken by the simulation as a function of the Cores
-plt.figure(figsize=(12, 4))
-
-# Subplot 1: Time vs Cores
-plt.subplot(1, 3, 1)
+# Plot 1: Time vs Cores
+plt.figure()
 plt.plot(processors, time, 'o-')
 plt.xlabel('# of cores')
 plt.ylabel('Time (s)')
 plt.title('Time')
 plt.grid()
+plt.savefig('time.png', dpi=300)
+#plt.show()
 
-# Subplot 2: Speedup vs Cores
-plt.subplot(1, 3, 2)
+# Plot 2: Speedup vs Cores
+plt.figure()
 plt.plot(processors, speedup, 'o-')
 plt.xlabel('# of cores')
 plt.ylabel('Speedup')
 plt.title('Speedup')
 plt.grid()
+plt.savefig('speedup.png', dpi=300)
+#plt.show()
 
-# Subplot 3: Efficiency vs Cores
-plt.subplot(1, 3, 3)
+# Plot 3: Efficiency vs Cores
+plt.figure()
 plt.plot(processors, efficiency, 'o-')
 plt.xlabel('# of cores')
 plt.ylabel('Efficiency')
 plt.title('Efficiency')
 plt.grid()
-
-plt.tight_layout()
-plt.savefig('cores.png', dpi=300)
-plt.show()
+plt.savefig('efficiency.png', dpi=300)
+#plt.show()
